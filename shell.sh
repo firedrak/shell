166210b4a57551d99b3ba656a39e1504e9cff5d0
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-21 14:10:35 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo rm -rf crawler
+sudo rm -rf ../crawler
 git clone https://github.com/firedrak/crawler.git
 sudo time crawler/python3 main.py https://raw.githubusercontent.com/firedrak/spider/main/template.py
 
