80d33c357ed2794ad9693ac875a3bd750ee74ccd
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-22 11:31:20 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,5 +2,5 @@
 sudo rm -rf crawler
 sudo git clone https://github.com/firedrak/crawler.git
 sudo pip install -r crawler/requirements.txt
-time python3 crawler/main.py https://gist.githubusercontent.com/firedrak/9bd24ba6a1f42d864c8a98b94cde4f36/raw/d4297a1025a270f3d61aadebd5dccfff29fac56b
+time python3 crawler/main.py 192.168.56.101 https://gist.githubusercontent.com/firedrak/9bd24ba6a1f42d864c8a98b94cde4f36/raw/d4297a1025a270f3d61aadebd5dccfff29fac56b
 
