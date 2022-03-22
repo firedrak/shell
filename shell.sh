bff5c02cac1ebe6f6c707c31718b040af1e7b6cf
firedrak <101325463+firedrak@users.noreply.github.com>
2022-03-22 15:09:24 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sudo rm -rf crawler
 sudo apt install python3-pip
+sudo apt install redis-tools
+sudo apt install redis-server
 sudo git clone https://github.com/firedrak/crawler.git
 sudo pip install -r crawler/requirements.txt
 time python3 crawler/main.py 172.16.221.234 https://gist.githubusercontent.com/firedrak/9bd24ba6a1f42d864c8a98b94cde4f36/raw/d4297a1025a270f3d61aadebd5dccfff29fac56b
